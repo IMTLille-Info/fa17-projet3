523944b0562016a9367b94ba3e4afd9ef14a1bf5
jean.castel <jean.castel@elv.telecom-lille.fr>
2015-03-07 16:52:48 +0100

    Ajout classe Serveur pour tests

---
+++ Jeux/src/server/Server.java
@@ -0,0 +1,13 @@
+package server;
+
+public class Server {
+
+	/**
+	 * @param String args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello World !! Je suis le serveur de Zelfa !!");
+	}
+
+}
