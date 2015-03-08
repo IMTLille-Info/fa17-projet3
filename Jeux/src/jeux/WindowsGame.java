8076c9f6a9cd1890d1708e442e337d18030ae5bf
nicolaspicart <nicolasalexandrep@ymail.com>
2015-03-08 21:12:49 +0100

    Modification de la création du personnage

--- Jeux/src/jeux/WindowsGame.java
+++ Jeux/src/jeux/WindowsGame.java
@@ -25,15 +25,16 @@ public class WindowsGame extends BasicGame {
 	private GameContainer container;
 	private TiledMap map;
 	private float x = 284, y = 276;
+	private static int ycarte = 600, xcarte = 600;
 	private int direction = 0;
 	private boolean moving = false;
-	private Animation[] animations = new Animation[8];
-	SpriteSheet spriteSheet;
+	Animation[] animations;
 	static Link link;
 
 	public static void main(String[] args) throws SlickException {
-		link = new Link("castwab.ddns.net", 2015);
-		AppGameContainer container = new AppGameContainer(new WindowsGame(), 600, 600, false);
+		//link = new Link("castwab.ddns.net", 2015);
+		// Création de la fenêtre de jeux
+		AppGameContainer container = new AppGameContainer(new WindowsGame(), xcarte, ycarte, false);
 		container.start();
 		
 	}
@@ -43,24 +44,31 @@ public class WindowsGame extends BasicGame {
 	}
 
 	@Override
-	public void render(GameContainer container, Graphics g) throws SlickException {
-		g.translate(container.getWidth() / 2 - (int) this.x, container.getHeight() / 2 - (int) this.y);
-		this.map.render(0, 0);
-		g.setColor(Color.black);
-		g.fillOval(x, y+35, 32, 16);
+	public void init(GameContainer container) throws SlickException {
+		this.container = container;
+		// Déclaration de la map
+		this.map = new TiledMap("resources/map/map2.tmx");
+		// Création d'un joueur
 		Player player = new Player();
-		Animation[] animations = player.CreateAnimation(new Image("resources/characters/char2.png"), 32, 48, 4);;
+		// Enregistrement des différentes positions et animations du perosonnage
+		animations = player.CreateAnimation(new Image("resources/characters/char2.png"), 32, 48, 4);;
+	}
+	
+	/*
+	 * Fonction qui gère le rendu pour chaque frame
+	 */
+	@Override
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		// Affichage de la carte
+		this.map.render(0, 0);
+		// Affichage des animations du personnages
 		g.drawAnimation(animations[direction + (moving ? 4 : 0)], x, y);
 		
 	}
 
-	@Override
-	public void init(GameContainer container) throws SlickException {
-		this.container = container;
-		this.map = new TiledMap("resources/map/map.tmx");
-	}
-
-
+	/*
+	 * Mise à jour du personnage
+	 */
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
 		if (this.moving) {
