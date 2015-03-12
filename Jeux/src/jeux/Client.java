fa856c522d22d6332873115546fcea243ce08f62
nicolaspicart <nicolasalexandrep@ymail.com>
2015-03-12 17:18:45 +0100

    Merge

--- Jeux/src/jeux/Client.java
+++ Jeux/src/jeux/Client.java
@@ -8,14 +8,14 @@ public class Client {
 	public static Socket socket = null;
 	public static Thread t1;
 	
-	public static void main(String[] args) {
+	public Client() {
 	
 		
 	try {
 		
 		System.out.println("Demande de connexion");
 		socket = new Socket("castwab.ddns.net",2015);
-		System.out.println("Connexion établie avec le serveur, authentification :"); // Si le message s'affiche c'est que je suis connecté
+		System.out.println("Connexion ��tablie avec le serveur, authentification :"); // Si le message s'affiche c'est que je suis connect��
 		
 		t1 = new Thread(new Connexion(socket));
 		t1.start();
@@ -23,9 +23,9 @@ public class Client {
 		
 		
 	} catch (UnknownHostException e) {
-	  System.err.println("Impossible de se connecter à l'adresse "+socket.getLocalAddress());
+	  System.err.println("Impossible de se connecter �� l'adresse "+socket.getLocalAddress());
 	} catch (IOException e) {
-	  System.err.println("Aucun serveur à l'écoute du port "+socket.getLocalPort());
+	  System.err.println("Aucun serveur �� l'��coute du port "+socket.getLocalPort());
 	}
 	
 	
