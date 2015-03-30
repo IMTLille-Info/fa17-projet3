89c8eae7fcd5edf41965d45ccad76a526ab934a9
nicolaspicart <nicolasalexandrep@ymail.com>
2015-03-30 15:27:54 +0200

    Correction bug déplacement de personnage

--- Jeux/src/jeux/WindowsGame.java
+++ Jeux/src/jeux/WindowsGame.java
@@ -1,4 +1,5 @@
 package jeux;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
@@ -8,8 +9,8 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 
 /**
- * @author NicolasPicart, JeanCastel
- * co author, qui n'a pas vraiment réussi à coder... AlexandreD
+ * @author NicolasPicart, JeanCastel co author, qui n'a pas vraiment réussi à
+ *         coder... AlexandreD
  */
 
 public class WindowsGame extends BasicGame {
@@ -23,23 +24,14 @@ public class WindowsGame extends BasicGame {
 	public Maps cases; // Maps avec les différentes cases
 
 	public static void main(String[] args) throws SlickException {
-<<<<<<< HEAD
-		//client = new Client("192.168.0.11", 2015);
 
-			AppGameContainer container = new AppGameContainer(new WindowsGame(),xcarte, ycarte, false); // Création de la fenêtre de jeux
-			container.setShowFPS(false); // Cacher l'affichage FPS
-			container.setVSync(true);	
-			container.setTargetFrameRate(60);
-			container.start();
-=======
-		//Thread client = new Thread(new Client("127.0.0.1", 2015));
-		//client.start();
+		// client = new Client("172.27.89.217", 2015);
 		AppGameContainer container = new AppGameContainer(new WindowsGame(),xcarte, ycarte, false); // Création de la fenêtre de jeux
 		container.setShowFPS(false); // Cacher l'affichage FPS
-		container.setVSync(true);	
+		container.setVSync(true);
 		container.setTargetFrameRate(60);
 		container.start();
->>>>>>> 8c2960850baeec9696bf303687bfd03fac66aedd
+
 	}
 
 	public WindowsGame() {
@@ -52,9 +44,18 @@ public class WindowsGame extends BasicGame {
 	@Override
 	public void init(GameContainer container) throws SlickException {
 		map = new TiledMap("resources/map/map2.tmx"); // Déclaration de la map
-		player1 = new Player(new Image("resources/characters/char2.png"), 32, 48, 4, 90, 90, 90, 5); // Création d'un joueur 
 		ecouteur = new CaseListener(container); // Ecouteurs sur chaque case
 		cases = new Maps(container, ecouteur); // Mise en place des cases
+		this.setPostionJour(10, 10);
+		player1 = new Player(new Image("resources/characters/char2.png"), 32, 48, 4, this.Newx, this.Newy, 90,  5); // Création d'un joueur
+		
+		
+		
+	}
+	
+	public void setPostionJour(int col, int ligne){
+		this.Newx = cases.get_coordonnees(col, ligne).getX();
+		this.Newy = cases.get_coordonnees(col, ligne).getY();
 	}
 
 	/*
@@ -62,18 +63,24 @@ public class WindowsGame extends BasicGame {
 	 */
 	@Override
 	public void render(GameContainer container, Graphics g) throws SlickException {
+		/*
+		 * Todo
+		 * Déclenchement action de déplacement du joueur à la reception des coordonnées données par le serveur.
+		 */
+		
 		map.render(0, 0); // Affichage de la carte (Visuel)
-		cases.drawMap(container,g); // Affichage des cases
-		player1.move(g, (float) Newx, (float) Newy); // Déplacement du joueur sur la carte
+		cases.drawMap(container, g); // Affichage des cases
+		player1.move(g, (float) Newx, (float) Newy); // Déplacement du joueur
+														// sur la carte
 	}
-	
+
 	@Override
-	public void mouseClicked(int button, int x, int y, int clickCount){
+	public void mouseClicked(int button, int x, int y, int clickCount) {
 		this.Newx = ecouteur.getX();
 		this.Newy = ecouteur.getY();
 		this.player1.setCurrentCol(ecouteur.getColonne());
 		this.player1.setCurrentLine(ecouteur.getLigne());
-		//client.send_pos(player1);
+		// client.send_pos(player1);
 	}
 
 	/*
